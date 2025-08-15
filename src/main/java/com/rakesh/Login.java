88e292f235b61d26fe34b7b406ec0cadc22d3d10
Kore Rakesh <korerakesh2004@gmail.com>
2025-08-15 18:45:13 +0530

    Update Login.java

--- src/main/java/com/rakesh/Login.java
+++ src/main/java/com/rakesh/Login.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpSession;
 
 @WebServlet("/Login")
 public class Login extends HttpServlet {
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		 
 		String uname=request.getParameter("uname");
